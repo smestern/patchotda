eefbc3636f96bcd8d2ece5592f1dbf07948a1239
smestern <52610944+smestern@users.noreply.github.com>
2024-06-15 11:02:55 +0000

    update title

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,8 +9,8 @@ import time
 import zipfile
 
 # Page title
-st.set_page_config(page_title='ML Model Building', page_icon='🤖')
-st.title('🤖 ML Model Building')
+st.set_page_config(page_title='Smestern: Map My Spikes Challenge 2024', page_icon='')
+st.title('Smestern: Map My Spikes Challenge 2024')
 
 with st.expander('About this app'):
   st.markdown('**What can this app do?**')
